b9b4bad3bbcfad884e0b67e1e36e78cf61700cd3
André Grüttner <a.gruettner@web.de>
2024-02-24 02:00:15 +0100

    dd gitconfig

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ mkdir -p "$XDG_CONFIG_HOME"/bash
 mkdir -p "$XDG_CONFIG_HOME"/alacritty
 mkdir -p "$XDG_CONFIG_HOME"/tmux
 mkdir -p "$XDG_CONFIG_HOME"/starship
+mkdir -p "$XDG_CONFIG_HOME"/git
 
 
 # addons
@@ -17,6 +18,7 @@ mkdir -p "$XDG_CONFIG_HOME"/starship
 # symbolic links
 ln -sf "$PWD/config/bash/.bash_profile" "$HOME"/.bash_profile
 ln -sf "$PWD/config/bash/.bashrc" "$HOME"/.bashrc
+ln -sf "$PWD/config/git/.gitconfig" "$HOME"/.gitconfig
 ln -sf "$PWD/config/alacritty/alacritty.toml" "$XDG_CONFIG_HOME"/alacritty/alacritty.toml
 ln -sf "$PWD/config/tmux/tmux.conf" "$XDG_CONFIG_HOME"/tmux/tmux.conf
 ln -sf "$PWD/config/starship/starship.toml" "$XDG_CONFIG_HOME"/starship.toml
