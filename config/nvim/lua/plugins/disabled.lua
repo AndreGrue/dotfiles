5d09f2b12f4a8d99f82fb40e89381f04492322b2
André Grüttner <a.gruettner@web.de>
2024-03-12 22:17:43 +0100

    comment

---
+++ config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+---
+return {
+  ---
+  { "echasnovski/mini.comment", enabled = false },
+  ---
+}
---
+++ config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,19 @@
+---
+return {
+
+  -- add themes
+  { "ellisonleao/gruvbox.nvim" },
+  { "navarasu/onedark.nvim" },
+  { "wittyjudge/gruvbox-material.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+      -- colorscheme = "gruvbox-material",
+      -- colorscheme = "onedark",
+      -- style = "darker",
+    },
+  },
+}
