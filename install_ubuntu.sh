0eb68828f156a2f1a0ca2a81ecf965ec292236ad
André Grüttner <a.gruettner@web.de>
2024-04-08 23:37:28 +0200

    ++

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -33,7 +33,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # neovim
 sudo apt-get install cargo chafa imagemagick
-#brew install npm
 #curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 #sudo rm -rf /opt/nvim
 #sudo tar -C /opt -xzf nvim-linux64.tar.gz
