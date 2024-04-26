75822dbfb76cbe2441128861283fa74bfebcbbec
André Grüttner <a.gruettner@web.de>
2024-04-26 17:56:10 +0200

    finish fold

--- config/nvim/lua/plugins/fold_ufo.lua
+++ config/nvim/lua/plugins/fold_ufo.lua
@@ -3,13 +3,68 @@
 --
 return {
   "kevinhwang91/nvim-ufo",
+  dependencies = { "kevinhwang91/promise-async" },
   event = "VeryLazy",
-  config = function()
+  opts = {
+    open_fold_hl_timeout = 150,
+    close_fold_kinds_for_ft = {
+      default = { "imports", "comment" },
+      json = { "array" },
+      c = { "comment", "region" },
+    },
+    preview = {
+      -- win_config = {
+      -- border = { "", "─", "", "", "", "─", "", "" },
+      -- winhighlight = "Normal:Folded",
+      -- winblend = 0,
+      -- },
+      mappings = {
+        scrollU = "<C-u>",
+        scrollD = "<C-d>",
+        jumpTop = "[",
+        jumpBot = "]",
+      },
+    },
+  },
+  config = function(_, opts)
+    --
+    --
     vim.o.foldcolumn = "1" -- '0' is not bad
     vim.o.foldlevel = 20 -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
-
+    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+    --
+    --
+    local handler = function(virtText, lnum, endLnum, width, truncate)
+      local newVirtText = {}
+      local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+      local sufWidth = vim.fn.strdisplaywidth(suffix)
+      local targetWidth = width - sufWidth
+      local curWidth = 0
+      for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+          table.insert(newVirtText, chunk)
+        else
+          chunkText = truncate(chunkText, targetWidth - curWidth)
+          local hlGroup = chunk[2]
+          table.insert(newVirtText, { chunkText, hlGroup })
+          chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          -- str width returned from truncate() may less than 2nd argument, need padding
+          if curWidth + chunkWidth < targetWidth then
+            suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+          end
+          break
+        end
+        curWidth = curWidth + chunkWidth
+      end
+      table.insert(newVirtText, { suffix, "MoreMsg" })
+      return newVirtText
+    end
+    --
+    --
     local ufo = require("ufo")
     -- Remap the builtin keymaps for keeping the foldlevel
     vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
@@ -48,7 +103,13 @@ return {
         -- you can add other fields for setting up lsp server in this table
       })
     end
-    require("ufo").setup()
+
+    --
+    -- global handler
+    -- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
+    -- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
+    --
+    opts["fold_virt_text_handler"] = handler
+    ufo.setup(opts)
   end,
-  dependencies = { "kevinhwang91/promise-async" },
 }
