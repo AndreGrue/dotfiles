8765417455c47e8b48dc186a939be11fcf7a3509
André Grüttner <a.gruettner@web.de>
2024-04-30 23:35:20 +0200

    add bqf

---
+++ config/nvim/lua/plugins/quickfixlist.lua
@@ -0,0 +1,15 @@
+---
+return {
+  {
+    "kevinhwang91/nvim-bqf",
+    event = "VeryLazy",
+    ft = "qf", -- load on quickfix list opened,
+    dependencies = {
+      "junegunn/fzf",
+      run = function()
+        vim.fn["fzf#install"]()
+      end,
+    },
+    opts = {},
+  },
+}
