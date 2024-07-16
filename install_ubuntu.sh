a1721795ac49b6ed58fe515b9ffd92defcee11a5
André Grüttner <a.gruettner@web.de>
2024-07-16 22:28:31 +0200

    fiiixxx

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -15,17 +15,17 @@ wget -P ~/.local/share/fonts $FONTPATH/$FONTPKG &&
 
 #############################################################################################
 # common
-sudo apt install fzf exa bat ripgrep zoxide entr thefuck
-sudo apt install mc ncdu btop htop
-sudo apt install curl wget rsync
-sudo apt install unzip gzip tar
-sudo apt install imagemagick libmagickwand-dev libgraphicsmagick1-dev chafa
+sudo apt-get install -y fzf eza bat ripgrep zoxide entr thefuck
+sudo apt-get install -y mc ncdu btop htop
+sudo apt-get install -y curl wget rsync
+sudo apt-get install -y unzip gzip tar
+sudo apt-get install -y imagemagick libmagickwand-dev libgraphicsmagick1-dev chafa
 
 # # fzf
 # git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 # ~/.fzf/install
 # fd
-sudo apt install fd-find
+sudo apt-get install -y fd-find
 ln -s $(which fdfind) ~/.local/bin/fd
 
 #############################################################################################
@@ -51,27 +51,35 @@ echo 'kitty.desktop' >~/.config/xdg-terminals.list
 # mkdir -p ~/.bash_completion
 # cp /usr/share/bash-completion/completions/alacritty ~/.bash_completion/alacritty
 # zsh
-sudo apt-get install zsh
+sudo apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # tmux
-sudo apt-get install tmux
+sudo apt-get install -y tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 #############################################################################################
 
 # latex
-sudo apt-get install bibtex
+sudo apt-get install -y bibtex
 
 # neovim
 #sudo add-apt-repository ppa:neovim-ppa/unstable
 #sudo apt-get update
 #sudo apt-get install neovim
-sudo apt-get install luarocks
+sudo apt-get install -y luarocks
 sudo luarocks install magick
 
-sudo apt-get install cargo
+sudo apt-get install -y cargo
 cargo install tree-sitter-cli
+
+sudo apt-get install -y npm
+sudo npm install -g neovim
+sudo npm install prettier --global
+sudo npm install markdownlint-cli2 --global
+sudo npm install markdown-toc --global
+
+sudo apt-get install -y python3-pip python3-venv python3-neovim
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
@@ -85,6 +93,6 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
-sudo apt install git git-delta
+sudo apt-get install -y git git-delta
 
 #brew install lazydocker
