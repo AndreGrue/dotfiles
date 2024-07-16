e1ab68066034e19b294e26934bbd3531f8fb92c7
André Grüttner <andre.gruettner@siemens.com>
2024-07-16 12:50:40 +0200

    kitty install desktop

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -37,6 +37,13 @@ ln -s $(which fdfind) ~/.local/bin/fd
 curl -sS https://starship.rs/install.sh | sh
 # kitty
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+echo 'kitty.desktop' >~/.config/xdg-terminals.list
+
 # alacritty
 # sudo add-apt-repository ppa:aslatter/ppa -y
 # sudo apt install alacritty
