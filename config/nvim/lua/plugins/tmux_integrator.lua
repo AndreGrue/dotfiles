e9aa4d091e366740836529c980c01ef1c6cf342d
André Grüttner <a.gruettner@web.de>
2025-07-08 22:14:48 +0200

    clean

--- config/nvim/lua/plugins/tmux_integrator.lua
+++ config/nvim/lua/plugins/tmux_integrator.lua
@@ -3,6 +3,7 @@ if true then return {} end
 
 return {
   "christoomey/vim-tmux-navigator",
+
   cmd = {
     "TmuxNavigateLeft",
     "TmuxNavigateDown",
@@ -10,6 +11,7 @@ return {
     "TmuxNavigateRight",
     "TmuxNavigatePrevious",
   },
+
   keys = {
     { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
     { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
