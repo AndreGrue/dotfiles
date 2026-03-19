db52ffc718e1af71908d7ae78362aac221a3b206
André Grüttner <a.gruettner@web.de>
2026-03-19 22:06:18 +0100

    hass mcp

--- config/copilot/install.sh
+++ config/copilot/install.sh
@@ -10,8 +10,13 @@ npm install -g @github/copilot
 npm install -g @modelcontextprotocol/server-filesystem
 npm install -g @modelcontextprotocol/server-sequential-thinking
 npm install -g @modelcontextprotocol/server-memory
-npm install -g mcp-ssh-manager
-
 # not needed if using uv
 # pip3 install mcp-server-fetch
 # pip3 install mcp-server-git
+
+# https://github.com/bvisible/mcp-ssh-manager
+# npm install -g mcp-ssh-manager
+# https://github.com/zibdie/SSH-MCP-Server
+# npm install -g @zibdie/ssh-mcp-server
+# https://github.com/classfang/ssh-mcp-server
+# npm install -g @fangjunjie/ssh-mcp-server
