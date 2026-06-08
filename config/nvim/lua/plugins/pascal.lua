cffa3fd8feb9196b2db4ce7b9a2257878a759a10
André Grüttner <a.gruettner@web.de>
2026-06-08 22:39:07 +0200

    +

---
+++ config/nvim/lua/plugins/pascal.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pasls = {
+          default_config = {
+            cmd = { "pasls" },
+            filetypes = { "pascal", "objectpascal" },
+            root_dir = require("lspconfig.util").root_pattern(".git", "."),
+          },
+        },
+      },
+      setup = {
+        pasls = function(_, opts)
+          require("lspconfig").pasls.setup(opts)
+          return true
+        end,
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      -- Register custom formatter
+      opts.formatters = vim.tbl_deep_extend("force", opts.formatters or {}, {
+        ptop = {
+          command = "ptop",
+          args = { "-c", vim.fn.expand("~/workspace/dotfiles/config/pascal/ptop.cfg"), "$FILENAME", "$FILENAME" },
+          stdin = false,
+          condition = function(ctx)
+            return vim.fn.executable("ptop") == 1
+          end,
+        },
+      })
+
+      -- Assign it to Pascal files
+      opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft or {}, {
+        pascal = { "ptop" },
+        objectpascal = { "ptop" },
+      })
+    end,
+  },
+  -- {
+  --   "alaviss/nim.nvim", -- basic Pascal-like TS syntax
+  --   ft = { "pascal" },
+  -- },
+}
---
+++ config/nvim/lua/plugins/sidekick.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/sidekick.nvim",
+  keys = {
+    {
+      "<leader>aa",
+      function()
+        require("sidekick.cli").toggle({ name = "opencode", focus = true })
+      end,
+      desc = "Sidekick Toggle OpenCode",
+    },
+  },
+}
